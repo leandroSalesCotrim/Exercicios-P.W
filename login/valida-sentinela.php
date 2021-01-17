c02e8800fb9aa3d19a492111edc1c1d294d6609c
Leandro de Sales Cotrim <56699403+leandroSalesCotrim@users.noreply.github.com>
2021-01-17 18:27:59 -0300

    Delete login directory

--- login/valida-sentinela.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    session_start();
-    if (($_SESSION['login-session'] != 'adm') || 
-        ($_SESSION['senha-session'] != '123')){
-            header("Location: ../index.php");
-        }
-?>
